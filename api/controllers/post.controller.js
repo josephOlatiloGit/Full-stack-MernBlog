a87dab7081f7cd2987b44bc7cb266775ec68f92e
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-08-07 13:02:32 +0100

    create get posts api routes

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -27,3 +27,117 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+/**
+ * Get All Post route:
+ * We need to make this route more dynamic so it can be
+ * called at different places in our application.(by getting the post with all attributes from our Model, as well as searchTerms)
+ * Also we want to add a show more button to render more post, we implement that by showing the post index.
+ */
+
+// export const getPosts = async (req, res, next) => {
+//   try {
+//     const startIndex = parseInt(req.query.startIndex) || 0;
+//     const limit = parseInt(req.query.limit) || 9;
+//     const sortDirection = req.query.order === "asc" ? 1 : -1;
+//
+// THIS CODE WILL RETURN A SERVER ERROR : source.toObject is not a function
+// The Post model should be used to interact with the database, not to construct query objects directly.
+//
+//     const posts = await Post.find({
+//       ...Post(req.query.userId && { userId: req.query.userId }),
+//       ...Post(req.query.category && { category: req.query.category }),
+//       ...Post(req.query.slug && { slug: req.query.slug }),
+//       ...Post(req.query.postId && { _id: req.query.postId }),
+//       ...Post(
+//         req.query.searchTerm && {
+//           $or: [
+//             { title: { $regex: req.query.searchTerm, $options: "i" } },
+//             { content: { $regex: req.query.searchTerm, $options: "i" } },
+//           ],
+//         }
+//       ),
+//     })
+//       .sort({ updatedAt: sortDirection })
+//       .skip(startIndex)
+//       .limit(limit);
+
+//     // getTotal post:
+//     const totalPosts = await Post.countDocuments();
+
+//     // getTotal Post in one month ago:
+//     const now = new Date();
+
+//     const oneMonthAgo = new Date(
+//       now.getFullYear(),
+//       now.getMonth() - 1,
+//       now.getDate()
+//     );
+
+//     const lastMonthPosts = await Post.countDocuments({
+//       createdAt: { $gte: oneMonthAgo },
+//     });
+
+//     return res.status(200).son({
+//       posts,
+//       totalPosts,
+//       lastMonthPosts,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    const query = {};
+
+    if (req.query.userId) query.userId = req.query.userId;
+    if (req.query.category) query.category = req.query.category;
+    if (req.query.slug) query.slug = req.query.slug;
+    if (req.query.postId) query._id = req.query.postId;
+    if (req.query.searchTerm) {
+      query.$or = [
+        { title: { $regex: req.query.searchTerm, $options: "i" } },
+        { content: { $regex: req.query.searchTerm, $options: "i" } },
+      ];
+    }
+
+    const posts = await Post.find(query)
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    // getTotal post:
+    const totalPosts = await Post.countDocuments(query);
+
+    // getTotal Post in one month ago:
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      ...query,
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    return res.status(200).json({
+      success: true,
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
