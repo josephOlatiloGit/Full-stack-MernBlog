3d67b2c6ed5b425a8dd556a0a311ed27505a394e
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-08-08 21:58:28 +0100

    add update post functionality

--- client/src/components/updatePost.jsx
+++ client/src/components/updatePost.jsx
@@ -1,5 +1,5 @@
 import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import {
@@ -11,10 +11,12 @@ import {
 import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 /**
- * We need to make this page private for only the Admin
+ * We need to make this Update post page private for only the Admin.
+ * We need useParams to get the particular postId. from react router-dom. So we fetch the post data based on the postId
  * So in the Private route component, we add another component that will make the create post page private for only the admin.
  *
  * Here we use a package called react-quill for the text input.
@@ -23,14 +25,37 @@ import { useNavigate } from "react-router-dom";
  * We implement the upload image code as we did with the profile page using firebase
  */
 
-export default function CreatePost() {
+export default function UpdatePost() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
   const [publishError, setPublishError] = useState(null);
-
+  const { postId } = useParams();
   const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
+
+  // Fetching the update post by the postId
+  useEffect(() => {
+    try {
+      const fetchPost = async () => {
+        const res = await fetch(`/api/post/getposts?postId=${postId}`);
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+          setPublishError(data.message);
+          return;
+        }
+        if (res.ok) {
+          setPublishError(null);
+          setFormData(data.posts[0]);
+        }
+      };
+      fetchPost();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [postId]);
 
   const handleUploadImage = async () => {
     try {
@@ -72,13 +97,16 @@ export default function CreatePost() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("/api/post/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const res = await fetch(
+        `/api/post/updatepost/${formData._id}/${currentUser._id}`,
+        {
+          method: "Put",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
       const data = await res.json();
       if (!res.ok) {
         setPublishError(data.message);
@@ -96,7 +124,7 @@ export default function CreatePost() {
 
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
-      <h1 className="text-center text-3xl my-7 font-semibold"></h1>
+      <h1 className="text-center text-3xl my-7 font-semibold">Update post</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
           <TextInput
@@ -108,11 +136,13 @@ export default function CreatePost() {
             onChange={(e) =>
               setFormData({ ...formData, title: e.target.value })
             }
+            value={formData.title}
           />
           <Select
             onChange={(e) =>
               setFormData({ ...formData, category: e.target.value })
             }
+            value={formData.category}
           >
             <option value={"uncategorized"}> Select a category</option>
             <option value={"javascript"}>JavaScript</option>
@@ -158,6 +188,7 @@ export default function CreatePost() {
         )}
         <ReactQuill
           theme="snow"
+          value={formData.content}
           placeholder="write something..."
           className="h-72 mb-12"
           required
@@ -166,7 +197,7 @@ export default function CreatePost() {
           }}
         />
         <Button type="submit" gradientDuoTone={"purpleToPink"}>
-          Publish
+          Update post
         </Button>
         {publishError && (
           <Alert className="mt-5" color={"failure"}>
