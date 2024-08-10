b8bd81000a3981c9a7e7c161eac1d663c1f00b64
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-08-10 19:07:14 +0100

    add call to action to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 /*We use the useParams hook to target each
  * post slug in order to fetch the post data from the api route based on the postSlug.
@@ -74,6 +75,9 @@ export default function PostPage() {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
     </main>
   );
 }
